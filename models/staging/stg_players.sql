c1d0d2729f00dd95574f555d0e20a048b89b1896
saitam.game@gmail.com <saitam.game@gmail.com>
2023-11-09 15:46:38 +0000

    modified 2 stg files created the view stats_per_season

--- models/staging/stg_players.sql
+++ models/staging/stg_players.sql
@@ -1,4 +1,10 @@
-SELECT 
-    *EXCEPT(date_of_birth),
-    CAST(date_of_birth AS DATE) as date_of_birth
-FROM `transfermarkt-391212.kaggle_data.players` 
+WITH sq1 AS (
+    SELECT 
+        *EXCEPT(date_of_birth),
+        CAST(date_of_birth AS DATE) as date_of_birth,
+    FROM `transfermarkt-391212.kaggle_data.players`)
+
+SELECT
+    *,
+    DATE_DIFF(CURRENT_DATE,date_of_birth,YEAR) as current_age
+FROM sq1
