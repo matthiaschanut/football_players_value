d75a25473150fd716efbc2b9eaa555ec1cfdbf79
saitam.game@gmail.com <saitam.game@gmail.com>
2023-11-10 17:52:29 +0000

    formatage dates

--- models/clean/clean_game_lineups.sql
+++ models/clean/clean_game_lineups.sql
@@ -4,6 +4,9 @@ SELECT
         WHEN t2.type = 'Substitutions' AND t1.player_id = t2.player_id THEN 'starting_lineup'
         WHEN t2.type = 'Substitutions' AND t1.player_id = t2.player_in_id THEN 'substitutes'
         ELSE t1.type
-    END as type
+    END as type,
+    t3.date
 FROM {{ ref('stg_game_lineups') }} t1
 LEFT JOIN {{ ref('stg_game_events') }} t2 ON t1.game_id = t2.game_id
+LEFT JOIN {{ ref('clean_games') }} t3 ON t1.game_id = t3.game_id
+ORDER BY t3.date ASC
--- models/staging/stg_appearances.sql
+++ models/staging/stg_appearances.sql
@@ -1,1 +1,4 @@
-SELECT * FROM `transfermarkt-391212.kaggle_data.appearances` 
+SELECT 
+    *EXCEPT(date),
+    CAST(date AS DATE) as date
+FROM `transfermarkt-391212.kaggle_data.appearances` 
